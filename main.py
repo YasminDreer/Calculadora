90a38e6539c373953bfff8206ee091777d06e662
YasminDreer <yasmimdreer_@hotmail.com>
2026-05-29 10:46:14 +0100

    Completar função
    Completar função da calculadora

--- main.py
+++ main.py
@@ -10,12 +10,21 @@ def calculadora(num1: float, num2: float, operador: str) -> float:
     result = float("nan")
     if operador == '+':
         result = num1 + num2
+    elif operador == '-':
+        result = num1 - num2
+    elif operador == '*':
+        result = num1 * num2
+    elif operador == '/':
+        result = num1 / num2
+    elif operador == '**':
+        result = num1 ** num2
+    elif operador == '%':
+        result = num1 % num2
 
     return result
 
 
 if __name__ == "__main__":
-
     while True:
         os.system('cls' if os.name == 'nt' else 'clear')
         try:
